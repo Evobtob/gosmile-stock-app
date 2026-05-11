93bc218367d52e045ae86c733ee97ff8c8967534
Nia Santos <nia_santos@Mac.lan>
2026-05-11 17:38:26 +0100

    Auto sync stock from Google Sheet on app start

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='gosmile-stock-v4';
-const ASSETS=['./','./index.html','./styles.css?v=undo1','./app.js?v=undo1','./manifest.json','./assets/icon.svg'];
+const CACHE='gosmile-stock-v5';
+const ASSETS=['./','./index.html','./styles.css?v=sync2','./app.js?v=sync2','./manifest.json','./assets/icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))]))});
 self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request)))});
