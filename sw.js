138a12859c9d51497336d509e40b7ee10cdb40f5
Nia Santos <nia_santos@Mac.lan>
2026-05-14 01:18:29 +0100

    fix: refresh PWA cache for Neodent theme

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='gosmile-stock-v7';
-const ASSETS=['./','./index.html','./styles.css?v=batch1','./app.js?v=batch1','./manifest.json','./assets/icon.svg'];
+const CACHE='gosmile-stock-v8-neodent-brand';
+const ASSETS=['./','./index.html','./styles.css?v=neodent-brand','./app.js?v=neodent-brand','./manifest.json','./assets/icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))]))});
 self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request)))});
